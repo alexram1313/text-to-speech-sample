0c83423f8d32bf5d402a357adf92b2a134d3bf6c
alexram1313 <alexir13@gmail.com>
2016-07-12 14:38:51 -0700

    Add files via upload

---
+++ load.py
@@ -0,0 +1,63 @@
+import re
+import wave
+import pyaudio
+import _thread
+import time
+
+class TextToSpeech:
+    
+    CHUNK = 1024
+
+    def __init__(self, words_pron_dict:str = 'cmudict-0.7b.txt'):
+        self._l = {}
+        self._load_words(words_pron_dict)
+
+    def _load_words(self, words_pron_dict:str):
+        with open(words_pron_dict, 'r') as file:
+            for line in file:
+                if not line.startswith(';;;'):
+                    key, val = line.split('  ',2)
+                    self._l[key] = re.findall(r"[A-Z]+",val)
+
+    def get_pronunciation(self, str_input):
+        list_pron = []
+        for word in re.findall(r"[\w']+",str_input.upper()):
+            if word in self._l:
+                list_pron += self._l[word]
+        print(list_pron)
+        delay=0
+        for pron in list_pron:
+            _thread.start_new_thread( TextToSpeech._play_audio, (pron,delay,))
+            delay += 0.145
+    
+    def _play_audio(sound, delay):
+        try:
+            time.sleep(delay)
+            wf = wave.open("sounds/"+sound+".wav", 'rb')
+            p = pyaudio.PyAudio()
+            stream = p.open(format=p.get_format_from_width(wf.getsampwidth()),
+                            channels=wf.getnchannels(),
+                            rate=wf.getframerate(),
+                            output=True)
+            
+            data = wf.readframes(TextToSpeech.CHUNK)
+            
+            while data:
+                stream.write(data)
+                data = wf.readframes(TextToSpeech.CHUNK)
+        
+            stream.stop_stream()
+            stream.close()
+
+            p.terminate()
+            return
+        except:
+            pass
+    
+ 
+ 
+
+if __name__ == '__main__':
+    tts = TextToSpeech()
+    while True:
+        tts.get_pronunciation(input('Enter a word or phrase: '))
